4b82394f972150befa59764432dee9eafe9ea1c2
Peleg Streit <pelegstreit@gmail.com>
2023-01-13 13:22:50 +0200

    first message

---
+++ src/styles/resetcss.css.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    html,
+    body,
+    #root{
+      min-height: 100%;
+    }
+    html {
+      font-size: 10px;
+    }
+    body {
+      background: papayawhip;
+      background: linear-gradient(to bottom,#f5f5dc 0%,#d2b48c 100%);
+    }
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+    li {
+      list-style-type: none;
+    }
+`;
+
+export default GlobalStyles;
